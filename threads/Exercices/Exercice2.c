430b8e81527b50035a7ae1ea781529d80ef20153
HEMELSDAEL Alexandre <alexandre.hemelsdael.etu@univ-lille.fr>
2025-01-09 13:46:21 +0100

    Ajout de l'exercice 2

---
+++ threads/Exercices/Exercice2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *bonjour(void *arg){
+  for(int i=0; i<100; i++){
+    printf("Bonjour\n");
+  }
+  printf("Le thread Bonjour a terminé\n");
+  pthread_exit(NULL);
+}
+
+void *aurevoir(void *arg){
+  for(int i=0; i<100; i++){
+    printf("Au revoir\n");
+  }
+  printf("Le thread Au revoir a terminé\n");
+  pthread_exit(NULL);
+}
+
+int main(){
+  pthread_t thread1, thread2;
+  int ret;
+  ret = pthread_create(&thread1, NULL, bonjour, NULL);
+  if(ret !=0){
+    perror("Erreur lors de la création du thread Bonjour");
+    exit(1);
+  }
+
+  ret = pthread_create(&thread2, NULL, aurevoir, NULL);
+  if(ret!=0){
+    perror("Erreur lors de la création du thread Au revoir");
+    exit(1);
+  }
+
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
+  return 0;
+}
